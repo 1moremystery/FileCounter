e07070bb5705efd1c09e28ccf35b65e1f7da8b6c
Levi <166890492+1moremystery@users.noreply.github.com>
2025-12-06 22:19:40 -0600

    RecentlyUsed functions

--- FileCounter/RecentlyUsed.cs
+++ FileCounter/RecentlyUsed.cs
@@ -1,14 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Xml;
 
 namespace FileCounter
 {
     public static class RecentlyUsed
     {
+        /// <summary>
+        /// Gets recent stuff
+        /// </summary>
+        /// <param name="recentFiles"></param>
+        /// <returns></returns>
+        public static string GetRecents(out List<string> recentFiles)
+        {
+            recentFiles = new();
+            if(!File.Exists("recent.xml")) return "";
+
+            using (XmlReader reader = XmlReader.Create("recent.xml"))
+            {
+                while (reader.Read())
+                {
+                    if(reader.IsStartElement() && reader.Name == "file")
+                    {
+                        recentFiles.Add(reader.ReadString());
+                    }
+                }
+            }
+
+            return recentFiles[0];
+        }
+
+        public static void WriteRecent(IEnumerable<string> recentFiles)
+        {
+            using(XmlWriter writer = XmlWriter.Create("recent.xml", new() { Indent = true }))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("root");
+                foreach (string file in recentFiles)
+                {
+                    writer.WriteStartElement("file");
+                    writer.WriteString(file);
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+
+            }
+        }
+
+        public static void AddToRecent(List<string> recentFiles, string nPath)
+        {
+            if (recentFiles.Remove(nPath))
+            {
+                recentFiles.Insert(0,nPath);
+            }
+            WriteRecent(recentFiles);
+        }
 
 
+        ////public static void RecentToMenuItem(List<string>recentFiles,MenuItem top)
+        ////{
+        ////    top.Items.Clear();
+        ////    foreach (string file in recentFiles)
+        ////    {
+
+        ////    }
+        ////}
     }
 }
